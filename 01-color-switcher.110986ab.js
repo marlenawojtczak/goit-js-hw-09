a58dca136065b6557cb87bab790e5f7f106c8700
marlenawojtczak <117003410+marlenawojtczak@users.noreply.github.com>
2023-04-12 21:24:13 +0000

    Deploying to gh-pages from @ marlenawojtczak/goit-js-hw-09@f691c6e8eab86b2afdd7dfe4340cce6b4a276db4 🚀

---
+++ 01-color-switcher.110986ab.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");const a=()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`};t.addEventListener("click",(()=>{intervalId=setInterval(a,1e3),t.disabled=!0})),e.addEventListener("click",(()=>{clearInterval(intervalId),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.110986ab.js.map
