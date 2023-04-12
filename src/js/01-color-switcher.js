f691c6e8eab86b2afdd7dfe4340cce6b4a276db4
marlenawojtczak <wojtczakmarlena100@gmail.com>
2023-04-12 23:23:12 +0200

    added first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const changeBgColor = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+};
+
+startBtn.addEventListener('click', () => {
+  intervalId = setInterval(changeBgColor, 1000);
+  startBtn.disabled = true;
+});
+
+stopBtn.addEventListener(`click`, () => {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+});
