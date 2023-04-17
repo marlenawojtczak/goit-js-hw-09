dcf0a2e43a7d5245877af1dbda8d19fc8635c625
marlenawojtczak <wojtczakmarlena100@gmail.com>
2023-04-17 23:07:52 +0200

    added task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const delayInput = parseInt(
+    document.querySelector('input[name="delay"]').value
+  );
+  const stepInput = parseInt(
+    document.querySelector('input[name="step"]').value
+  );
+  const amountInput = parseInt(
+    document.querySelector('input[name="amount"]').value
+  );
+
+  for (let i = 0; i < amountInput; i++) {
+    createPromise(i + 1, delayInput + i * stepInput)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
   }
-}
+
+  function createPromise(position, delay) {
+    return new Promise((resolve, reject) => {
+      const shouldResolve = Math.random() > 0.3;
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, delay);
+    });
+  }
+});
